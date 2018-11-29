226dfbc07c4cf0aa32e58cd2a1b87536d4e085dd
FWS <fws@MacBook-Pro-5.local>
2018-11-29 15:33:29 +0100

    feat(article): Add Article Use case

--- app/Http/Controllers/ListArticlesController.php
+++ app/Http/Controllers/ListArticlesController.php
@@ -4,10 +4,25 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers;
 
+use LaravelDay\Article\UseCase\ListArticles\ListArticles;
+
 class ListArticlesController extends Controller
 {
     public function __invoke()
     {
-        return response()->json([]);
+        $listArticles = new ListArticles(); //Use case abstraction
+        $articles = $listArticles();
+
+        return response()->json($articles);
+
+        /*
+        return response()->json([
+            [
+                'title' => 'Articolo di prova',
+                'body' => 'Questo è il testo di un articolo di prova',
+                'creationDate' => '2018-11-29 00:00:00',
+            ]
+        ]);
+        */
     }
 }
